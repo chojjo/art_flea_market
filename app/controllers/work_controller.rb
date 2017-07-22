0b98cd6f55c80136ecf3854ad0bba27c41d1f31b
Hyoun Cho <0706gusl@gmail.com>
2017-07-22 09:04:37 +0000

    work controller and model

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -1,2 +1,27 @@
 class WorkController < ApplicationController
+    
+    def new
+        
+        @categories=["회화", "조각", "공예"]
+        @work=Work.new
+    end
+    
+    def create
+        
+        @checked_item = []
+            (0..6). each do |i|
+              if params["checkbox-" + i.to_s].present?
+                @checked_item << params["checkbox-" + i.to_s]
+              end
+            end
+        
+        @name=params[:name]
+    
+        @description=params[:description]
+        
+        @work.name=@name
+        @work.description=@description
+        @work.category=@checked_item
+        
+    end
 end
